4c6a4b032c62ac8ba5156ee2d20c3b502d4d5b15
alexandria-perry <aeperry22@gmail.com>
2018-11-28 15:04:11 -0800

    Define change_video and change_video_list actions

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,19 @@
 var changeVideo = (video) => ({
-  //TODO:  Return some action object to change the currently playing video.
+  type: 'CHANGE_VIDEO',
+  video: video
 });
 
 export default changeVideo;
+
+
+// {
+//   type: 'CHANGE_VIDEO',
+//   newVideo: video
+// }
+
+
+// reducer = function(currentState, action) {
+//   if (action.type === 'CHANGE_VIDEO') {
+//     // set new state's currentVideo to be action.newVideo
+//   }
+// }
