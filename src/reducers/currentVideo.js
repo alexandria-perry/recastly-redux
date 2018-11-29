29b0c691fe9110f0615e815dd87a674a3420d04a
alexandria-perry <aeperry22@gmail.com>
2018-11-29 11:48:07 -0800

    Refactor / clean up

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,15 +1,5 @@
 import Redux from 'redux';
 
-var dummyVideo = {
-  id: {
-    videoId: 'dummy videoId'
-  },
-  snippet: {
-    title: 'dummy title',
-    description: 'dummy description'
-  }
-};
-
 var currentVideoReducer = (state = null, action) => {
   if (action.type === 'CHANGE_VIDEO') {
     return action.video;
